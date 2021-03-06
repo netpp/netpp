e47e24c2186d7729df00011ead7fa834cfe94897
whywhywhywhygo <whywhywhywhygo@gmail.com>
2021-03-06 19:03:07 +0800

    mock epoll and test event handle operations

--- test/MockSysCallEnvironment.cpp
+++ test/MockSysCallEnvironment.cpp
@@ -8,12 +8,26 @@ extern "C" {
 #include <sys/socket.h>
 }
 
-EpollCreate action_epoll_create;
-EpollCreate action_epoll_create1;
-EpollWait action_epoll_wait;
-EpollPWait action_epoll_pwait;
-EpollCtl action_epoll_ctl;
-Close action_close;
+int SysCall::mock_epoll_create(int size)
+{ return MockSysCallEnvironment::real_epoll_create(size); }
+
+int SysCall::mock_epoll_create1(int flags)
+{ return MockSysCallEnvironment::real_epoll_create1(flags); }
+	
+int SysCall::mock_epoll_wait(int epfd, struct epoll_event *events, int maxevents, int timeout)
+{ return MockSysCallEnvironment::real_epoll_wait(epfd, events, maxevents, timeout); }
+
+int SysCall::mock_epoll_pwait(int epfd, struct epoll_event *events, int maxevents, int timeout, const sigset_t *sigmask)
+{ return MockSysCallEnvironment::real_epoll_pwait(epfd, events, maxevents, timeout, sigmask); }
+
+int SysCall::mock_epoll_ctl(int epfd, int op, int fd, struct epoll_event *event)
+{ return MockSysCallEnvironment::real_epoll_ctl(epfd, op, fd, event); }
+
+int SysCall::mock_close(int fd)
+{ return MockSysCallEnvironment::real_close(fd); }
+
+SysCall *MockSysCallEnvironment::defaultSysMock;
+SysCall *MockSysCallEnvironment::sysMock;
 
 EpollCreate MockSysCallEnvironment::real_epoll_create;
 EpollCreate MockSysCallEnvironment::real_epoll_create1;
@@ -24,6 +38,8 @@ Close MockSysCallEnvironment::real_close;
 
 void MockSysCallEnvironment::SetUp()
 {
+	defaultSysMock = new SysCall();
+	sysMock = defaultSysMock;
 	// compiler will use custome defined functions firstly 
 	real_epoll_create = reinterpret_cast<EpollCreate>(::dlsym(RTLD_NEXT, "epoll_create"));
 	real_epoll_create1 = reinterpret_cast<EpollCreate>(::dlsym(RTLD_NEXT, "epoll_create1"));
@@ -31,48 +47,46 @@ void MockSysCallEnvironment::SetUp()
 	real_epoll_pwait = reinterpret_cast<EpollPWait>(::dlsym(RTLD_NEXT, "epoll_pwait"));
 	real_epoll_ctl = reinterpret_cast<EpollCtl>(::dlsym(RTLD_NEXT, "epoll_ctl"));
 	real_close = reinterpret_cast<Close>(::dlsym(RTLD_NEXT, "close"));
+}
 
-	restoreSysCall();
+void MockSysCallEnvironment::registerMock(SysCall *mock)
+{
+	sysMock = mock;
 }
 
 void MockSysCallEnvironment::restoreSysCall()
 {
-	::action_epoll_create = real_epoll_create;
-	::action_epoll_create1 = real_epoll_create1;
-	::action_epoll_wait = real_epoll_wait;
-	::action_epoll_pwait = real_epoll_pwait;
-	::action_epoll_ctl = real_epoll_ctl;
-	::action_close = real_close;
+	sysMock = defaultSysMock;
 }
 
 extern "C" {
 int epoll_create(int size)
 {
-	return ::action_epoll_create(size);
+	return MockSysCallEnvironment::sysMock->mock_epoll_create(size);
 }
 
 int epoll_create1(int flags)
 {
-	return ::action_epoll_create1(flags);
+	return MockSysCallEnvironment::sysMock->mock_epoll_create1(flags);
 }
 
 int epoll_wait(int epfd, struct epoll_event *events, int maxevents, int timeout)
 {
-	return ::action_epoll_wait(epfd, events, maxevents, timeout);
+	return MockSysCallEnvironment::sysMock->mock_epoll_wait(epfd, events, maxevents, timeout);
 }
 
 int epoll_pwait(int epfd, struct epoll_event *events, int maxevents, int timeout, const sigset_t *sigmask)
 {
-	return ::action_epoll_pwait(epfd, events, maxevents, timeout, sigmask);
+	return MockSysCallEnvironment::sysMock->mock_epoll_pwait(epfd, events, maxevents, timeout, sigmask);
 }
 
 int epoll_ctl(int epfd, int op, int fd, struct epoll_event *event)
 {
-	return ::action_epoll_ctl(epfd, op, fd, event);
+	return MockSysCallEnvironment::sysMock->mock_epoll_ctl(epfd, op, fd, event);
 }
 
 int close(int fd)
 {
-	return ::action_close(fd);
+	return MockSysCallEnvironment::sysMock->mock_close(fd);
 }
 }
--- test/MockSysCallEnvironment.h
+++ test/MockSysCallEnvironment.h
@@ -7,7 +7,19 @@ using EpollCreate = int(*)(int);
 using EpollWait = int(*)(int, struct epoll_event *, int, int);
 using EpollPWait = int(*)(int, struct epoll_event *, int, int, const sigset_t *);
 using EpollCtl = int(*)(int, int, int, struct epoll_event *);
-using Close = int(*)(int fd);
+using Close = int(*)(int);
+
+class SysCall {
+public:
+	virtual ~SysCall() = default;
+
+	virtual int mock_epoll_create(int size);
+	virtual int mock_epoll_create1(int flags);
+	virtual int mock_epoll_wait(int epfd, struct epoll_event *events, int maxevents, int timeout);
+	virtual int mock_epoll_pwait(int epfd, struct epoll_event *events, int maxevents, int timeout, const sigset_t *sigmask);
+	virtual int mock_epoll_ctl(int epfd, int op, int fd, struct epoll_event *event);
+	virtual int mock_close(int fd);
+};
 
 class MockSysCallEnvironment : public testing::Environment {
 public:
@@ -17,9 +29,13 @@ public:
 	void SetUp() override;
 	void TearDown() override {}
 
+	static void registerMock(SysCall *mock);
 	static void restoreSysCall();
 
-private:
+public:
+	static SysCall *defaultSysMock;
+	static SysCall *sysMock;
+
 	static EpollCreate real_epoll_create;
 	static EpollCreate real_epoll_create1;
 	static EpollWait real_epoll_wait;
@@ -27,12 +43,5 @@ private:
 	static EpollCtl real_epoll_ctl;
 	static Close real_close;
 };
-
-extern EpollCreate action_epoll_create;
-extern EpollCreate action_epoll_create1;
-extern EpollWait action_epoll_wait;
-extern EpollPWait action_epoll_pwait;
-extern EpollCtl action_epoll_ctl;
-extern Close action_close;
 
 #endif
